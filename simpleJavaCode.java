44bc5d40a4b3cf437a3288018f02601bf8fd64dc
kr625 <68609630+kr625@users.noreply.github.com>
2020-12-18 07:49:02 +0530

    Update simpleJavaCode.java
    Made a simple change and doing my second commit

--- simpleJavaCode.java
+++ simpleJavaCode.java
@@ -1,5 +1,6 @@
 public class simpleJavaCode {
 public static void main(String args[]) {
 System.out.println("Hi, This is a sample Java Code!!!");
+System.out.println("Printing line 2.updated the code in github");
 }
 }
